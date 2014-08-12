14e2fcd4c68b0de764f908e3db199698ba61ce86
ShadowEric <eric@shadowhealth.com>
2014-08-12 19:14:45 -0400

    Exercises 7.6

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,6 +25,13 @@ describe "UserPages" do
 			it "should not create a user" do
 				expect {click_button submit }.not_to change(User, :count)
 			end
+			#exercise 7.6 #2 -- *add more tests here*
+			describe "after submission" do
+				before { click_button submit }
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+			end
+			
 		end
 		describe "with valid information" do
 			before do
@@ -36,6 +43,14 @@ describe "UserPages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
     		end
+    		#exercise 7.6 #3
+    		describe "after saving the user" do
+    			before { click_button submit }
+    			let(:user) { User.find_by(email: 'user@example.com') }
+    			it { should have_title(user.name) }
+    			it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    		end
+
 		end
 	end
 end
